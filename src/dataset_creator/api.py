69144557daa34b1a00172140a00362c72031b6e5
carlosp420 <mycalesis@gmail.com>
2015-09-15 10:28:23 +0300

    overview

--- src/dataset_creator/api.py
+++ src/dataset_creator/api.py
@@ -1,3 +1,6 @@
+from .creator import Creator
+
+
 class Dataset(object):
     """
     User's class for making datasets of several formats. It needs as input a
@@ -10,10 +13,15 @@ class Dataset(object):
         * sequences
     """
     def __init__(self, seq_records):
-        self.dataset_str = None
+        self.dataset_str = self.create_dataset()
+        self.genes = None
+        self.number_chars = None
+        self.data = self._prepare_data()
 
     def create_dataset(self):
-        self.dataset_str = '#NEXUS dataset'
+        creator = Creator(self.data)
+        dataset_str = creator.dataset_str
+        return dataset_str
 
     def extract_genes(self):
         pass
@@ -22,4 +30,7 @@ class Dataset(object):
         pass
 
     def extract_number_of_taxa(self):
+        pass
+
+    def _prepare_data(self):
         pass
---
+++ src/dataset_creator/creator.py
@@ -0,0 +1,15 @@
+class Creator(object):
+    def __init__(self, data):
+        self.dataset_str = None
+
+    def create_dataset_blocks(self):
+        pass
+
+    def create_dataset_header(self):
+        pass
+
+    def create_dataset_footer(self):
+        pass
+
+    def create_extra_dataset_file(self):
+        pass
