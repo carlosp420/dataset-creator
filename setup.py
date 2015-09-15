39d84f3943b61b2e46f710176586f77d5d926e4c
carlosp420 <mycalesis@gmail.com>
2015-09-15 15:31:52 +0300

    update requires

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'seqrecord-expanded==0.1.2',
     ],
     extras_require={
         # eg:
