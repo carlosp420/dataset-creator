350de3fabf423ea868e78333537a0dd4002ec9a9
carlosp420 <mycalesis@gmail.com>
2015-09-21 12:07:18 +0300

    coverage

--- dataset_creator/bp_count.py
+++ dataset_creator/bp_count.py
@@ -16,22 +16,41 @@ class BasePairCount(object):
     """
     def __init__(self, reading_frame=None, codon_positions=None, partitioning=None,
                  count_start=None, count_end=None):
-        self._reading_frame = reading_frame
-        self._codon_positions = codon_positions
-        self._partitioning = partitioning
-        self._count_start = count_start
-        self._count_end = count_end
+        self._reading_frame = self._set_reading_frame(reading_frame)
+        self._codon_positions = self._set_codon_positions(codon_positions)
+        self._partitioning = self._set_partitioning(partitioning)
+        self._count_start = self._set_count_start(count_start)
+        self._count_end = self._set_count_end(count_end)
 
-        if self._reading_frame is None:
+    def _set_reading_frame(self, reading_frame):
+        if not reading_frame:
             raise ValueError("_reading_frame argument is needed. Can't be None")
-        elif self._codon_positions is None:
+        else:
+            return reading_frame
+
+    def _set_codon_positions(self, codon_positions):
+        if not codon_positions:
             raise ValueError("_codon_positions argument is needed. Can't be None")
-        elif self._partitioning is None:
+        else:
+            return codon_positions
+
+    def _set_partitioning(self, partitioning):
+        if not partitioning:
             raise ValueError("_partitioning argument is needed. Can't be None")
-        elif self._count_start is None:
+        else:
+            return partitioning
+
+    def _set_count_start(self, count_start):
+        if not count_start:
             raise ValueError("codon_start argument is needed. Can't be None")
-        elif self._count_end is None:
+        else:
+            return count_start
+
+    def _set_count_end(self, count_end):
+        if not count_end:
             raise ValueError("codon_end argument is needed. Can't be None")
+        else:
+            return count_end
 
     def get_corrected_count(self):
         if self._codon_positions == '1st-2nd' and self._partitioning == 'by codon position':
