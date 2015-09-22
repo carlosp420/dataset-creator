a4dc601c062233c92e389c47e2d58d805adbd337
carlosp420 <mycalesis@gmail.com>
2015-09-22 10:32:21 +0300

    move bp_count tests to nexus test file

--- tests/test_nexus.py
+++ tests/test_nexus.py
@@ -4,6 +4,7 @@ import unittest
 from .data import test_data
 from dataset_creator.dataset import Dataset
 from dataset_creator.nexus import DatasetFooter
+from dataset_creator.nexus import BasePairCount
 
 
 NEXUS_DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Nexus')
@@ -88,3 +89,28 @@ set partition = GENES;
         expected = open(test_data_file, 'r').read()
         result = dataset.dataset_str
         self.assertEqual(expected, result)
+
+
+class TestBpCount(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_parameter_reading_frame(self):
+        self.assertRaises(ValueError, BasePairCount, codon_positions='1st-2nd',
+                          partitioning='by codon position', count_start=100, count_end=512)
+
+    def test_parameter_codon_positions(self):
+        self.assertRaises(ValueError, BasePairCount, reading_frame=1,
+                          partitioning='by codon position', count_start=100, count_end=512)
+
+    def test_parameter_partitioning(self):
+        self.assertRaises(ValueError, BasePairCount, codon_positions='1st-2nd',
+                          reading_frame=1, count_start=100, count_end=512)
+
+    def test_parameter_count_start(self):
+        self.assertRaises(ValueError, BasePairCount, codon_positions='1st-2nd',
+                          partitioning='by codon position', reading_frame=1, count_end=512)
+
+    def test_parameter_count_end(self):
+        self.assertRaises(ValueError, BasePairCount, codon_positions='1st-2nd',
+                          partitioning='by codon position', count_start=100, reading_frame=1)
