2ad5117de05e231dd55e08a07c4c978651909adb
carlosp420 <mycalesis@gmail.com>
2015-09-22 12:05:02 +0300

    Use NEXUS format as base format
    Then we can use BioPython tools to conver to FASTA and Phylip formats.

--- dataset_creator/creator.py
+++ dataset_creator/creator.py
@@ -24,6 +24,10 @@ class Creator(object):
 
         >>> dataset_creator = Creator(data, format='NEXUS', codon_positions='ALL',
         ...                           partitioning='by gene')
+        >>> dataset_creator.dataset_str
+        '#NEXUS
+        blah blah
+        '
     """
     def __init__(self, data, format=None, codon_positions=None, partitioning=None):
         self.data = data
@@ -36,25 +40,21 @@ class Creator(object):
         self.dataset_str = self.put_everything_together()
 
     def create_dataset_header(self):
-        if self.format == 'NEXUS':
-            return nexus.dataset_header(self.data)
-
-        if self.format == 'PHYLIP':
-            return phylip.dataset_header(self.data)
+        return nexus.dataset_header(self.data)
 
     def create_dataset_block(self):
-        if self.format == 'NEXUS':
-            return nexus.DatasetBlock(self.data, self.codon_positions,
-                                      self.partitioning).dataset_block()
+        return nexus.DatasetBlock(self.data, self.codon_positions,
+                                  self.partitioning).dataset_block()
 
     def create_dataset_footer(self):
-        if self.format == 'NEXUS':
-            return nexus.DatasetFooter(self.data, codon_positions=self.codon_positions,
-                                       partitioning=self.partitioning).dataset_footer()
+        return nexus.DatasetFooter(self.data, codon_positions=self.codon_positions,
+                                   partitioning=self.partitioning).dataset_footer()
 
     def create_extra_dataset_file(self):
         pass
 
     def put_everything_together(self):
-        return '{0}\n\n{1}\n\n{2}'.format(self.dataset_header, self.dataset_block,
-                                          self.dataset_footer)
+        if self.format == 'NEXUS':
+            return '{0}\n\n{1}\n\n{2}'.format(self.dataset_header,
+                                              self.dataset_block,
+                                              self.dataset_footer)
