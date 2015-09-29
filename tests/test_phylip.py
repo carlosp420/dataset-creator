1f12bbcef18a7b29fc4316c989e42c4f3801d8e2
carlosp420 <mycalesis@gmail.com>
2015-09-29 14:55:53 +0300

    test data

--- tests/test_phylip.py
+++ tests/test_phylip.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import unittest
 
@@ -11,15 +12,16 @@ PHYLIP_DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Phy
 class TestPhylip(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
+        self.data = copy.copy(test_data)
 
     def test_dataset(self):
-        dataset = Dataset(test_data, format='PHYLIP', partitioning='by gene')
+        dataset = Dataset(self.data, format='PHYLIP', partitioning='by gene')
         result = dataset.dataset_str
         expected = open(os.path.join(PHYLIP_DATA_PATH, 'dataset.phy')).read()
         self.assertEqual(expected, result)
 
     def test_charset_file(self):
-        dataset = Dataset(test_data, format='PHYLIP', partitioning='by gene')
+        dataset = Dataset(self.data, format='PHYLIP', partitioning='by gene')
         result = dataset.extra_dataset_str
         expected = open(os.path.join(PHYLIP_DATA_PATH, 'charset_block_file.txt')).read()
         self.assertEqual(expected, result)
--- tests/test_tnt.py
+++ tests/test_tnt.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import unittest
 
@@ -11,9 +12,10 @@ TNT_DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Tnt')
 class TestTnt(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
+        self.data = copy.copy(test_data)
 
     def test_dataset(self):
-        dataset = Dataset(test_data, format='TNT', partitioning='by gene')
+        dataset = Dataset(self.data, format='TNT', partitioning='by gene')
         result = dataset.dataset_str
         expected = open(os.path.join(TNT_DATA_PATH, 'dataset.tnt')).read()
         self.assertEqual(expected, result)
