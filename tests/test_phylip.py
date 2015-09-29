d4dfedf6690f82ae676ce8e6c1552292cb3dbc71
carlosp420 <mycalesis@gmail.com>
2015-09-29 15:14:44 +0300

    use sample data from txt file

--- tests/test_phylip.py
+++ tests/test_phylip.py
@@ -1,18 +1,29 @@
-import copy
+import json
 import os
 import unittest
 
+from seqrecord_expanded import SeqRecordExpanded
+
 from dataset_creator.dataset import Dataset
 
-from .data import test_data
-
 PHYLIP_DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Phylip')
+SAMPLE_DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sample_data.txt')
 
 
 class TestPhylip(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
-        self.data = copy.copy(test_data)
+
+        with open(SAMPLE_DATA_PATH, 'r') as handle:
+            sample_data = json.loads(handle.read())
+        self.data = []
+        append = self.data.append
+
+        for i in sample_data:
+            seq_record = SeqRecordExpanded(i['seq'], voucher_code=i['voucher_code'],
+                                           taxonomy=i['taxonomy'], gene_code=i['gene_code'],
+                                           reading_frame=i['reading_frame'], table=i['table'])
+            append(seq_record)
 
     def test_dataset(self):
         dataset = Dataset(self.data, format='PHYLIP', partitioning='by gene')
