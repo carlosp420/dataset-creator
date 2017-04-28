f34d2ede5879810ba6120ff58ca91d6fe16c3dfb
carlosp420 <mycalesis@gmail.com>
2017-04-27 21:14:50 -0500

    added test

--- tests/test_datasetblock.py
+++ tests/test_datasetblock.py
@@ -38,3 +38,23 @@ class TestDatasetBlock(unittest.TestCase):
         expected = "_Lepidoptera_Nymphalidae_Satyrinae_Satyrini_Euptychiina_Euptychia"
         result = dataset_block.flatten_taxonomy(seq_record)
         self.assertEqual(expected, result)
+
+    def test_flatten_taxonomy__no_taxonomy(self):
+        seq = {
+            'seq': '????????????',
+            'voucher_code': "CP100-11",
+            'gene_code': 'ef1a',
+            'reading_frame': 2,
+            'table': 1,
+        }
+        dataset_block = DatasetBlock(data="", codon_positions="ALL", partitioning="")
+        seq_record = SeqRecordExpanded(
+            seq['seq'],
+            voucher_code=seq['voucher_code'],
+            gene_code=seq['gene_code'],
+            reading_frame=seq['reading_frame'],
+            table=seq['table'],
+        )
+        expected = ""
+        result = dataset_block.flatten_taxonomy(seq_record)
+        self.assertEqual(expected, result)
